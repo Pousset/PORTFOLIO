bbc10b06bd97f6390715f415ae4cc0afc340cbbb
Pousset <62299589+Pousset@users.noreply.github.com>
2024-09-08 07:20:37 +0200

    Add Download PDF

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./styles/App.css";
 import "./assets/contacts.svg";
 import "./assets/house-solid.svg";
 import main from "./components/Home";
+import PDF from "./components/downloadPDF";
 
 function App() {
   return (
     <div>
+      <div> {PDF} </div>
       <div>{main()}</div>
     </div>
   );

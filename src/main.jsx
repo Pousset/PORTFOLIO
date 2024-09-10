151da3cf45dbb13662abbb2628fd18fe0e046bb1
M.PIETTE <62299589+Pousset@users.noreply.github.com>
2024-09-10 06:05:12 +0200

    restore

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import './styles/Index.css';
 import ContactCard from './components/contactCard.jsx';
 
-const rootElement = document.getElementById('root');
-
-ReactDOM.render(
-  <GoogleOAuthProvider clientId="704617051875-amhllq6mh5pjrt41nvitc6bvra2nt58n.apps.googleusercontent.com">
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </GoogleOAuthProvider>,
-  rootElement
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
